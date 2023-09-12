d9b1159f67e2432b543124d1eaef797c59dd423d
excaliburXD <144137514+excaliburXD@users.noreply.github.com>
2023-09-12 20:16:15 +0700

    Tw version & Fixing Screen Height

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,15 +153,14 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
-TW_Y_OFFSET := 20
-TW_H_OFFSET := -40
+TARGET_SCREEN_HEIGHT := 2460
 TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_FRAMERATE := 60
 
 # Maintainer/Version
-TW_DEVICE_VERSION := Gilangeagea
+TW_DEVICE_VERSION := By_Gilangeagea
 
 # logcd
 TWRP_INCLUDE_LOGCAT := true
