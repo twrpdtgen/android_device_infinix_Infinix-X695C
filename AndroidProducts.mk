1e4c4281d81f7890f491d572581d80a22234fb5a
excaliburXD <144137514+excaliburXD@users.noreply.github.com>
2023-09-12 13:56:55 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,6 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X695C.mk
+    $(LOCAL_DIR)/twrp_X695C.mk
 
-COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X695C-user \
-    omni_Infinix-X695C-userdebug \
-    omni_Infinix-X695C-eng
+COMMON_LUNCH_CHOICES := twrp_X695C-eng
